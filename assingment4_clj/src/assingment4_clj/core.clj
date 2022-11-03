74897add88c01cae24abff6463fe02665aa839e2
Dante Viscuso <92130795+Dante945@users.noreply.github.com>
2022-11-02 21:47:55 -0700

    Changed comments

--- assingment4_clj/src/assingment4_clj/core.clj
+++ assingment4_clj/src/assingment4_clj/core.clj
@@ -5,26 +5,21 @@
 (defn quicksort
   "Divide and Conquer algorithm that uses a pivot"
   [coll]
-  ;; Base case of length == 1 or 0
   (if (< (count coll) 2)
     coll
     ;; Define pivot to be the first element of the collection
     (let [pivot (first coll)
-          ;; left will be all elements less than the pivot
           left (filter #(< % pivot) (rest coll))
-          ;; right will be all elements greater than the pivot
           right (filter #(<= pivot %) (rest coll))]
-      ;; Recursively call quick-sort on left and right and combine them
+      ;; lazy-cat doesn't process until we need it 
       (lazy-cat (quicksort left) [pivot] (quicksort right)))))
 
 ;; Part 2
 (defn mergesort
   "Divide and Conquer algorithm that merges two halves"
   [coll]
-  ;; Base case of length == 1 or 0
   (if (< (count coll) 2)
     coll
-    ;; Break the list into two halves
     (let [[left right] (split-at (/ (count coll) 2) coll)]
       ;; Merge procedure
       (loop
@@ -34,7 +29,6 @@
         (cond
           (empty? left) (lazy-cat temp right)
           (empty? right) (lazy-cat temp left)
-          ;; Add the smaller of the left and right halves and recur
           :else (if (<= (first left) (first right))
                   (recur (conj temp (first left)) (rest left) right)
                   (recur (conj temp (first right)) left (rest right))))))))
